4f466e6d407aa84b3b7c69a9e9a8247fd5016fbe
Letícia Santtos C <santoscastroleticia@gmail.com>
2026-04-29 21:53:52 -0300

    Adicionado comandos de inserir e limpar texto e delay

--- cypress/e2e/integration/elements.cy.js
+++ cypress/e2e/integration/elements.cy.js
@@ -37,4 +37,32 @@ describe('Work with basic elements', () => {
         cy.get('#resultado')
             .should('have.text', 'Voltou!')
     })
+
+    it.only('TextFields', () => {
+        //escreve o texto no campo
+        cy.get('#formNome').type('Cypress Test')
+        //encontra o campo pelo valor dentro dele
+        cy.get('#formNome').should('have.value', 'Cypress Test')
+
+        //acessa caixa de texto multilinhas
+        // é usado duas barras por conta dos dois pontos, se não da erro de execução
+        cy.get('#elementosForm\\:sugestoes')
+            .type('Texto texto texto texto texto texto')
+            .should('have.value', 'Texto texto texto texto texto texto')
+
+        //acessa campos da tabela acessando o id dela e passando pelos filhos
+        cy.get('#tabelaUsuarios > :nth-child(2) > :nth-child(1) > :nth-child(6) > input')
+                .type('Campo da tabela')
+
+        //{backspace} serve para apagar o último caractere escrito
+        cy.get('[data-cy="dataSobrenome"]')
+                .type('Teste12345{backspace}{backspace}')
+                .should('have.value', 'Teste123')
+
+        cy.get('#elementosForm\\:sugestoes')
+            .clear()
+            .type('Erro{selectall}acerto', {delay: 100})
+            .should('have.value', 'acerto')
+
+    })
 })
